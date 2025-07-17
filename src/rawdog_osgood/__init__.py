305ffe3db5b7c798c01af247d330a1d5d1f9dc19
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-07-17 19:55:15 -0400

    updated init py file

--- src/rawdog_osgood/__init__.py
+++ src/rawdog_osgood/__init__.py
@@ -1,1 +1,7 @@
 from .client import RawdogClient
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("rawdog_osgood")
+except PackageNotFoundError:
+    __version__ = "v0.0.0"
