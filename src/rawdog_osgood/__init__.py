d195a0a883bb78d08369f5a62fae7791f03fbc20
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-12-14 18:23:34 -0500

    updated version stamp in init file

--- src/rawdog_osgood/__init__.py
+++ src/rawdog_osgood/__init__.py
@@ -4,4 +4,4 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("rawdog_osgood")
 except PackageNotFoundError:
-    __version__ = "v0.0.0"
+    __version__ = "v0.0.1"
